a4a550bfc863f201469281800f54a1da4d66ac33
FattahulAlim <fathalim1512@gmail.com>
2024-12-03 20:41:53 +0700

    menambah fungsi inputan

--- mainfungsi.java
+++ mainfungsi.java
@@ -39,4 +39,64 @@ public class mainfungsi {
             }
         }
     }
+
+    static void inputan() {
+        Scanner sc = new Scanner(System.in);
+
+        String keputusan;
+        int j = 0;
+        int total = 0;
+        int sksSementara;
+
+        System.out.println("--- Tambah data KRS ---");
+        System.out.print("Nama Mahasiswa: ");
+        DataMhs[i][j] = sc.nextLine();
+        j++;
+
+        System.out.print("NIM: ");
+        DataMhs[i][j] = sc.nextLine();
+        while (true) {
+            j = 2;
+
+            System.out.print("Kode Mata Kuliah: ");
+            DataMhs[i][j] = sc.nextLine();
+            j++;
+
+            System.out.print("Nama Mata Kuliah: ");
+            DataMhs[i][j] = sc.nextLine();
+
+            System.out.print("Jumlah SKS(1-3): ");
+            sksSementara = sc.nextInt();
+            sc.nextLine();
+            if (sksSementara >= 1 && sksSementara < 4) {
+                sks[i] = sksSementara;
+                total += sks[i];
+            } else if (total > 24) {
+                System.out.println("Mohon input ulang (total sks tidak boleh lebih dari 24)");
+                return;
+            } else {
+                System.out.println("Mohon input ulang. (SKS tidak boleh melebihi 3)");
+                DataMhs[i][j] = null;
+                DataMhs[i][j-1] = null;
+                DataMhs[i][j-2] = null;
+                DataMhs[i][j-3] = null;
+                return;
+            }
+            System.out.println("Data berhasil ditambah!");
+            System.out.print("Tambah mata kuliah lain? (y/t): ");
+            keputusan = sc.nextLine();
+
+            j = 0;
+            if (keputusan.equalsIgnoreCase("t")) {
+                break;
+            } else {
+                i++;
+                DataMhs[i][j] = DataMhs[i-1][0];
+                j = 1;
+                DataMhs[i][j] = DataMhs[i-1][1];
+            }
+        } if (total <= 20) {
+            krsMhs++;
+        } System.out.println("Total sks yang diambil: " + total);
+    }
 }
